e9bc31ccbcc53faf4e510a92c32bc030d1ed9d6b
Mark Freeman <dorsettri@gmail.com>
2020-11-14 16:29:31 +0000

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,4 @@
+const name = 'Mark';
+// clg = shortcut for console.log, you can then hit ' to cteate a string and overtype object. Very fast!!
+// cer = console.error
+//nfn is a named function and then yo ucan tab through
